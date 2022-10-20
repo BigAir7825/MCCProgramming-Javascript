715f20658a67174a0e867dda163fdef03ff15c7a
Will Adair <88009623+BigAir7825@users.noreply.github.com>
2022-10-20 11:54:32 -0500

    Assignment 6 - Javascript Programming

---
+++ assign6/assignment06/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+    <head>
+        <title>INFO 2340 - Assignmennt 6</title>
+    </head>
+    <body>
+        <h1>Hello, World!</h1>
+        <div>Allow me to introduce myself! My name is <?php echo "John Doe!" ?></div>
+        <div>I am <?php echo "45" ?> years old and I am a student at Metropolitan Community College.</div>
+        <div>I am currently studying <?php echo "Botany" ?> and I hope to graduate in <?php echo "2020."?></div>
+</body>
