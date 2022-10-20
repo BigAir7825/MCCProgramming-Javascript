80d462d5a4f7ca127edf185597b61b189f2d24a7
Will Adair <88009623+BigAir7825@users.noreply.github.com>
2022-10-20 11:52:28 -0500

    Assignment 1 - Javascript Programming

---
+++ assign1/assignment01/assignment01.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#first-name").text("Will");
+    $("#last-name").text("Adair");
+    $(".mcc").css("color", "blue");
+    $(".mcc").css("font-style", "italic")
+    $("#hobby").text("Swimming");
+    $("#hobby").append("<li>Video Games</li>");
+});
